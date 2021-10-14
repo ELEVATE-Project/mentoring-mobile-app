34b880710c8f5294c2e1084d0a24222874702a00
rejneesh1 <pillairejneesh@gmail.com>
2021-10-14 12:34:12 +0530

    http service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { LocalStorageService } from './services';
+import { HTTP } from '@ionic-native/http/ngx';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
-    TranslateModule
-  ],
-  providers:[LocalStorageService]
+  imports: [CommonModule, TranslateModule],
+  providers: [LocalStorageService,HTTP],
 })
-export class CoreModule { }
+export class CoreModule {}
