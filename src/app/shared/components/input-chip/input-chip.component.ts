271361622a4b2a7c2ccf4f8a1836f597f563206f
rejneesh1 <pillairejneesh@gmail.com>
2021-10-18 21:57:46 +0530

    dynamic chip in form added

---
+++ src/app/shared/components/input-chip/input-chip.component.ts
@@ -0,0 +1,96 @@
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-input-chip',
+  templateUrl: './input-chip.component.html',
+  styleUrls: ['./input-chip.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: InputChipComponent,
+    },
+  ],
+})
+export class InputChipComponent implements OnInit, ControlValueAccessor {
+  @Input() label;
+  touched = false;
+  disabled = false;
+  chips = [
+    {
+      name: 'deo',
+      label: 'District education officer',
+    },
+    {
+      name: 'beo',
+      label: 'Block education officer',
+    },
+    {
+      name: 'hm',
+      label: 'Head Master',
+    },
+  ];
+  selectedChips;
+  _selectAll;
+
+  constructor() {}
+
+  onChange = (quantity) => {};
+
+  onTouched = () => {};
+
+  ngOnInit() {}
+
+  writeValue(value: any[]) {
+    this.selectedChips = new Set(value);
+  }
+  registerOnChange(onChange: any) {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: any) {
+    this.onTouched = onTouched;
+  }
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+  onChipClick(chip) {
+    this.markAsTouched();
+    if (this.disabled) {
+      return;
+    }
+    if (this.selectedChips.has(chip)) {
+      this.selectedChips.delete(chip);
+    } else {
+      this.selectedChips.add(chip);
+    }
+    if (!this.selectedChips.size) {
+      this.onChange('');
+    } else {
+      this.onChange([...this.selectedChips]);
+    }
+    if (this.selectedChips.size !== this.chips.length) {
+      this._selectAll = false;
+    } else {
+      this._selectAll = true;
+    }
+  }
+
+  selectAll() {
+    if (this._selectAll) {
+      this.chips.map((chip) => this.selectedChips.add(chip.name));
+    } else {
+      this.selectedChips.clear();
+    }
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DynamicFormComponent } from './components/dynamic-form/dynamic-form.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SessionCardComponent } from './components/session-card/session-card.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { InputChipComponent } from './components/input-chip/input-chip.component';
 @NgModule({
-  declarations: [DynamicFormComponent,SessionCardComponent,PageHeaderComponent],
-  imports: [CommonModule, ReactiveFormsModule, IonicModule,TranslateModule],
-  exports: [DynamicFormComponent,SessionCardComponent,PageHeaderComponent],
+  declarations: [
+    DynamicFormComponent,
+    SessionCardComponent,
+    PageHeaderComponent,
+    InputChipComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule, IonicModule, TranslateModule,FormsModule],
+  exports: [
+    DynamicFormComponent,
+    SessionCardComponent,
+    PageHeaderComponent,
+    InputChipComponent,
+  ],
 })
 export class SharedModule {}
