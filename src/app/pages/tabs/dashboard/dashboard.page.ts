cf6d84b131ac722d3c9f3424aace7599c6ab043b
afnantl <afnantl@tunerlabs.com>
2021-12-10 18:20:12 +0530

    STORY #725 Dashboard

--- src/app/pages/tabs/dashboard/dashboard.page.ts
+++ src/app/pages/tabs/dashboard/dashboard.page.ts
@@ -10,8 +10,8 @@ import { ProfileService } from 'src/app/core/services/profile/profile.service';
   styleUrls: ['dashboard.page.scss']
 })
 export class DashboardPage implements OnInit {
-  segment: any = 'mentee';
-  dataAvailable = true;
+  segment: any;
+  dataAvailable;
   isMentor:boolean;
   selectedFilter = "WEEKLY";
   filter: any = [
@@ -53,32 +53,17 @@ export class DashboardPage implements OnInit {
     }
 
   ionViewWillEnter(){
+    this.dataAvailable = false;
     if(typeof this.isMentor === "undefined"){
       this.profile.profileDetails(false).then(profileDetails => {
         this.isMentor = profileDetails?.isAMentor;
+        this.segment= this.isMentor ? "mentor":"mentee";
         this.getReports();
       })
     } else {
-      console.log("in else");
       this.getReports();
     }
   }
-
-  // setChartData({labels = [], values = []}) {
-  //   this.chartData = {
-  //     chart: {
-  //       data: {
-  //         labels: labels,
-  //         datasets: [
-  //           {
-  //             data: values,
-  //             backgroundColor: ['#ffab00', 'blue']
-  //           }
-  //         ]
-  //       }
-  //     }
-  //   };
-  // }
 
   segmentChanged(ev: any) {
     console.log('Segment changed', ev);
@@ -101,29 +86,20 @@ export class DashboardPage implements OnInit {
     this.loading = true;
     const url = this.segment === 'mentor' ? urlConstants.API_URLS.MENTOR_REPORTS : urlConstants.API_URLS.MENTEE_REPORTS;
     const config = {
-      url: url+this.selectedFilter,
+      url: url+this.selectedFilter.toUpperCase(),
     };
-    console.log(config);
     this.apiService.get(config).then(success => {
       let chartObj;
-      // this.chartData.chart.data.labels.length = 0;
-      // this.chartData.chart.data.datasets.data.length = 0;
+        this.chartData.chart.data.labels.length = 0;
+        this.chartData.chart.data.datasets[0].data.length = 0;
       if(this.segment === 'mentor'){
         this.chartData.chart.data.labels.push("Total Sessions Created", "Total Sessions Hosted")
-        this.chartData.chart.data.datasets.data.push(success.result.totalSessionCreated || 0, success.result.totalsessionHosted || 0);
-        // chartObj = {
-        //   labels:["Total Sessions Created", "Total Sessions Hosted"],
-        //   values: [success.result.totalSessionCreated || 0, success.result.totalsessionHosted | 0]
-        // }
+        this.chartData.chart.data.datasets[0].data.push(success.result.totalSessionCreated || 0, success.result.totalsessionHosted || 0);
       } else {
         this.chartData.chart.data.labels.push("Total Sessions Enrolled", "Total Sessions Attended")
-        this.chartData.chart.data.datasets.data.push(success.result.totalSessionEnrolled || 0, success.result.totalsessionsAttended || 0)
-        // chartObj = {
-        //   labels:["Total Sessions Enrolled", "Total Sessions Attended"],
-        //   values: [success.result.totalSessionEnrolled || 0, success.result.totalsessionsAttended || 0]
-        // }
+        this.chartData.chart.data.datasets[0].data.push(success.result.totalSessionEnrolled || 0, success.result.totalsessionsAttended || 0);
       }
-      // this.setChartData(chartObj);
+      this.dataAvailable=(this.chartData.chart.data.datasets[0].data[0]==0&&this.chartData.chart.data.datasets[0].data[1]==0) ? false:true;
       this.loading = false;
     }).catch(error => {
       this.loading = false;
