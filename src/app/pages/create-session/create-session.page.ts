b5294d1d85f424608ea17a2b962e2e677e59c72c
Ashwini Kumar <ashwini@tunerlabs.com>
2023-05-22 15:27:42 +0530

    create session prefillData

--- src/app/pages/create-session/create-session.page.ts
+++ src/app/pages/create-session/create-session.page.ts
@@ -162,8 +162,8 @@ export class CreateSessionPage implements OnInit {
         this.form1.myForm.markAsPristine();
         let result = await this.sessionService.createSession(form, this.id);
         this.sessionDetails = result;
-        this.type = 'segment';
         if (result._id) {
+          this.type = 'segment';
           this.id = result._id;
           result.startDate = moment.unix(result.startDate).format("YYYY-MM-DDTHH:mm");
           result.endDate = moment.unix(result.endDate).format("YYYY-MM-DDTHH:mm");
