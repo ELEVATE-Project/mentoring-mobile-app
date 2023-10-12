5579ab3394d8227cfd7b5c5b31218d921f15589e
GSuma21 <21gsuma@gmail.com>
2023-10-12 18:09:56 +0530

    issue #feat mentor directory and entor profile page api + ui related changes ( some keys are missing in mentor directory api endpoint )

--- src/app/core/constants/urlConstants.ts
+++ src/app/core/constants/urlConstants.ts
@@ -9,7 +9,7 @@ export const urlConstants = {
         PROFILE_DETAILS: "/user/v1/user/read/",
         MENTOR_PROFILE_DETAILS: "/mentoring/v1/mentors/profile/",
         MENTEE_PROFILE_DETAILS:"/mentoring/v1/mentees/profile/",
-        MENTORS_DIRECTORY:"/user/v1/mentors/list?page=",
+        MENTORS_DIRECTORY:"/mentoring/v1/users/list?type=mentor&page=",
         FILE_UPLOAD:"/user/v1/cloud-services/file/upload",
         SESSIONS:"/mentoring/v1/mentees/sessions?enrolled=", //sessions?enrolled=true/false&page=1&limit=5&search=:search
         HOME_SESSION:"/mentoring/v1/mentees/homeFeed?page=", ///v1/mentees/homefeed?page=1&limit=4
