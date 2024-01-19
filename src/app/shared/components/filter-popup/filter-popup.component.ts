c272deb32fe67c483621972fd87bb93e89aef75c
GSuma21 <21gsuma@gmail.com>
2024-01-19 10:32:57 +0530

    issue @fix filter change

--- src/app/shared/components/filter-popup/filter-popup.component.ts
+++ src/app/shared/components/filter-popup/filter-popup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 
 @Component({
@@ -7,6 +7,7 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./filter-popup.component.scss'],
 })
 export class FilterPopupComponent implements OnInit {
+  @Input() filterData: any;
   selectedFilters:any;
   constructor(private modalCtrl: ModalController) { }
 
