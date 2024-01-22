669755bf644dc9a371ad5d017bd15b025cd5a53c
GSuma21 <21gsuma@gmail.com>
2024-01-22 13:28:34 +0530

    issue #fix search popover and edit sessin in manage session page

--- src/app/shared/components/search-popover/search-popover.component.ts
+++ src/app/shared/components/search-popover/search-popover.component.ts
@@ -106,7 +106,7 @@ export class SearchPopoverComponent implements OnInit {
   }
 
   onCLickEvent(data: any) {
-    switch(data.action.name){
+    switch(data.action){
       case 'add':
         if(!this.control.meta .multiSelect){
           this.popoverController.dismiss([data.element], this.control)
@@ -131,6 +131,6 @@ export class SearchPopoverComponent implements OnInit {
  async onPaginatorChange(data:any) {
     this.page = data.page;
     this.limit = data.pageSize 
-    await this.list()
+    this.tableData = await this.list()
   }
 }
