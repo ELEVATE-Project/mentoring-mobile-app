b5396febb8cb221d83c6b8f5ee5e6d6747c4b8e6
Rahul K R <rahul@tunerlabs.com>
2024-01-31 19:28:14 +0530

    permission check issue resolved.

--- src/app/core/services/permission/permission.service.ts
+++ src/app/core/services/permission/permission.service.ts
@@ -12,11 +12,7 @@ export class PermissionService {
   constructor(private localStorage: LocalStorageService){}
 
   async hasPermission(permissions: any): Promise <boolean> {
-    if(!this.userPermissions.length){
-      await this.fetchPermissions().then((res) =>{
-        this.userPermissions = res;
-      })
-    }
+    await this.fetchPermissions();
     for (let userPermission of this.userPermissions) {
       if (permissions && userPermission.request_type.length && permissions.module === userPermission.module) {
         if (permissions.action.every((value: any) => userPermission.request_type.includes(value))) {
@@ -34,6 +30,7 @@ export class PermissionService {
         this.localStorage.getLocalData(localKeys.USER_DETAILS)
           .then(async (data) => {
             if(data) {
+              this.userPermissions = data?.permissions;
               resolve(data?.permissions);
             }
           })
