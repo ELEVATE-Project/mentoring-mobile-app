1eac33facafeb05d5b97f8e3f9db25519621f271
Ameen <syedameenabrar786@gmail.com>
2024-02-27 16:13:23 +0530

    sidebar private module

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -1,32 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, TitleCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
-
 import { PrivatePage } from './private.page';
-
 import {
   TranslateLoader,
   TranslateModule,
-  TranslateService,
 } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { HttpClient } from '@angular/common/http';
-import { RouteReuseStrategy, RouterModule } from '@angular/router';
-
+import { RouteReuseStrategy } from '@angular/router';
 import { PrivatePageRoutingModule } from './private-routing.module';
 export const translateHttpLoaderFactory = (httpClient: HttpClient) =>
   new TranslateHttpLoader(httpClient, './assets/i18n/', '.json');
-
 @NgModule({
+  declarations: [PrivatePage],
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
     PrivatePageRoutingModule,
-    TranslateModule.forRoot({
+    TranslateModule.forChild({
       loader: {
         provide: TranslateLoader,
         useFactory: translateHttpLoaderFactory,
@@ -35,7 +29,6 @@ export const translateHttpLoaderFactory = (httpClient: HttpClient) =>
     }),
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-  ],
-  declarations: [PrivatePage]
+  ]
 })
 export class PrivatePageModule {}
