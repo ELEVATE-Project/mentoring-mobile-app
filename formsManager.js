72ae98834edb6a59f8832e538d1f5b3677101ba4
5Amogh <amoghavarsh@navadhiti.com>
2024-07-05 17:33:53 +0530

    forms migration experimental code

---
+++ formsManager.js
@@ -0,0 +1,60 @@
+const axios = require("axios");
+
+const authToken = process.env.AUTH_TOKEN;
+const apiUrl = process.env.API_URL;
+
+const forms = require("forms.json");
+
+const createForm = async (form) => {
+  try {
+    const response = await axios.post(
+      `${apiUrl}/mentoring/v1/form/create`,
+      { form },
+      { headers: { Authorization: `Bearer ${authToken}` } }
+    );
+    if(response){
+        console.log("Form created successfully:", response.data);
+    }
+  } catch (error) {
+    console.error("Error creating form:", error);
+  }
+};
+
+const updateForm = async (form) => {
+  try {
+    const response = await axios.put(
+      `${apiUrl}/mentoring/v1/form/update`,
+      { form },
+      { headers: { Authorization: `Bearer ${authToken}` } }
+    );
+    if(response){
+        console.log("Form updated successfully:", response.data);
+    }
+  } catch (error) {
+    console.error("Error updating form:", error);
+  }
+};
+
+const createOrUpdateForms = async () => {
+  for (const form of forms) {
+    try {
+      switch (form.action) {
+        case "skip":
+          console.log("Skipping form with type", form.type );
+          break;
+        case "update":
+          await updateForm(form);
+          break;
+        case "create":
+          await createForm(form);
+          break;
+        default:
+          console.log("Unknown action for form with type", form.type);
+      }
+    } catch (error) {
+      console.error("Error processing form with type", form.type, ":", error);
+    }
+  }
+};
+
+createOrUpdateForms();
