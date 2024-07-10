31947112716404925a623fd0adc5f3a89c7e4759
Haritest <haridasksd@gmail.com>
2024-07-10 15:09:19 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,4 +4,7 @@ export const environment = {
   dev: false,
   baseUrl: 'your api url',
   sqliteDBName: 'DB',
+  restictedPages: ["EDIT_PROFILE", "PROFILE_PAGE", "LOGIN_ACTIVITY_PAGE", "CHANGE_PASSWORD_PAGE", "LANGUAGE_PAGE", "HELP_VIDEOS_PAGE", "HELP_PAGE", "OTP_PAGE", "RESET_PASSWORD_PAGE", "REGISTER_PAGE", "LOGIN_PAGE", "LANDING_PAGE"],
+  isAuthBypassed: true,
+  unauthorizedRedirectUrl: "/auth/login",
 };
