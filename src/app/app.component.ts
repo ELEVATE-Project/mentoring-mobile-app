f4a9487742876d09505a8203d5411f29e5e1f813
Cafnanc <cafnanc@gmail.com>
2024-07-24 12:09:16 +0530

    added console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,7 @@ export class AppComponent {
     if(Capacitor.isNativePlatform()){
       this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT); 
     }
-    environment.baseUrl = localStorage.getItem('X-auth-token') ? localStorage.getItem('X-auth-token') : environment.baseUrl;
+    environment.baseUrl = localStorage.getItem('baseUrl') ? localStorage.getItem('baseUrl') : environment.baseUrl;
     console.log(environment.baseUrl, "app comp")
   }
 
