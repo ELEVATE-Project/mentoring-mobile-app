ecc7fedf53f78bd32e001ba0694407e70ca3cae5
Cafnanc <cafnanc@gmail.com>
2024-07-26 21:29:26 +0530

    extra header

--- src/app/core/services/http/http.service.ts
+++ src/app/core/services/http/http.service.ts
@@ -51,7 +51,7 @@ export class HttpService {
       'accept-language':acceptLanguage
     }
     this.extraHeaders = JSON.parse(localStorage.getItem('headers'))
-    console.log(this.extraHeaders)
+    console.log("extra headers", this.extraHeaders)
     if(this.extraHeaders) {
       Object.keys(this.extraHeaders).forEach(key => {
         headers[key] = this.extraHeaders[key];
