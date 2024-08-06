4b33fe639a1f3935fda5170a4aad1c06c6f679d6
Afnan C <98315899+Cafnanc@users.noreply.github.com>
2024-08-06 14:06:36 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,7 @@ export class AppComponent {
     if(Capacitor.isNativePlatform()){
       this.screenOrientation.lock(this.screenOrientation.ORIENTATIONS.PORTRAIT); 
     }
-    environment.baseUrl = localStorage.getItem('baseUrl') ? localStorage.getItem('baseUrl') : environment.baseUrl;
-    console.log(environment.baseUrl, "app comp")
+    window['env']['baseUrl'] = localStorage.getItem('baseUrl') ? localStorage.getItem('baseUrl') : window['env']['baseUrl'];
   }
 
   ngOnInit(){
