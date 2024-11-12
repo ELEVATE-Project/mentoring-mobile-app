3fdd5d7bcad82ef8e758a5c6fdcd6ccaf547b670
Kiran Haridas <kiranharidas@apples-mbp.lan>
2024-11-12 18:08:37 +0530

    env.js file added

--- src/assets/env/env.js
+++ src/assets/env/env.js
@@ -1,18 +1,19 @@
 window["env"] = {
-    production: true,
-    name: 'debug environment',
-    staging: false,
-    dev: false,
-    baseUrl: 'base url',
-    sqliteDBName: 'db name',
-    deepLinkUrl: 'deep link',
-    privacyPolicyUrl:'privacy policy',
-    termsOfServiceUrl:'t&c',
-    supportEmail: "",
-    recaptchaSiteKey:"",
-    password:{
-      minLength:10,
-      rejectPattern:"",
-      errorMessage:""
-    },
+  production: false,
+  name: 'debug environment',
+  baseUrl: 'BASE_URL',
+  sqliteDBName: 'DB_NAME',
+  deepLinkUrl: 'DEEPLINK_URL',
+  privacyPolicyUrl:'PRIVACY_POLICY',
+  termsOfServiceUrl:'TERMS_OF_SERVICE_URL',
+  supportEmail: "SUPPORT_URL",
+  recaptchaSiteKey:"",
+  restictedPages: [],
+  isAuthBypassed: false,
+  unauthorizedRedirectUrl: "REDIRECT_URL",
+  password:{
+    minLength:10,
+    regexPattern: "^(?=(?:.*[A-Z]){2})(?=(?:.*[0-9]){2})(?=(?:.*[!@#%$&()\\-`.+,]){3}).{11,}$",
+    errorMessage:"Password should contain at least one uppercase letter, one number and one special character."
+  },
 };
