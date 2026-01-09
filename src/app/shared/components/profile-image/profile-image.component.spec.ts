885e1ada65c95639c744458368216ce156b0e969
rohxnn <rohanreji123@gmail.com>
2026-01-09 11:24:02 +0530

    fix profile image test

--- src/app/shared/components/profile-image/profile-image.component.spec.ts
+++ src/app/shared/components/profile-image/profile-image.component.spec.ts
@@ -1,6 +1,3 @@
-import 'zone.js';          
-import 'zone.js/testing';  
-
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 import { ProfileImageComponent } from './profile-image.component';
@@ -33,9 +30,7 @@ describe('ProfileImageComponent', () => {
         { provide: UtilService, useValue: mockUtilService }
       ]
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(ProfileImageComponent);
     component = fixture.componentInstance;
 
