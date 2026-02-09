c710d938b2857eb9a252afcf382f09a0606611ea
vishwanath1004 <50612405+vishwanath1004@users.noreply.github.com>
2026-02-09 16:21:11 +0530

    TATA strive changes

--- src/app/shared/components/search-popover/search-popover.component.ts
+++ src/app/shared/components/search-popover/search-popover.component.ts
@@ -18,6 +18,7 @@ export class SearchPopoverComponent implements OnInit {
     { name: 'name', displayName: 'Name', type: 'text' },
     { name: 'designation', displayName: 'Designation', type: 'array' },
     { name: 'organization', displayName: 'Organisation', type: 'text' },
+    { name: 'location', displayName: 'Location', type: 'text' },
     { name: 'email', displayName: 'E-mail ID', type: 'text' },
     { name: 'type', displayName: 'Enrollment type', type: 'text' },
     { name: 'action', displayName: 'Actions', type: 'button' }
